f71a7603887d5780fd3ed1db3b2a553c5c1b1f72
muhammadshahbaz08 <muhamadshahbaz17@gmail.com>
2021-07-24 23:52:34 +0500

    Add Some CSS to App Component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React, { useState } from "react";
 import { Message } from "./message";
-import './App.css'
+import "./App.css";
 
 export const App = () => {
   let [count, setCount] = useState(0);
   let [isMorning, setMorning] = useState(true);
   return (
-    <div className={`box ${isMorning ? 'dayTime' : ''}`}>
-      <h1>Good {isMorning ? 'Morning' : 'Night'}</h1>
-      <Message counter={count}/>
-      <button onClick={() => setCount(++count)}>Update Count</button>
-      <button onClick={() => setMorning(!isMorning)}>Update Day</button>
+    <div className={`box ${isMorning ? "dayTime" : "nightTime"}`}>
+      <h1>Good {isMorning ? "Morning" : "Night"}</h1>
+      <Message counter={count} />
+      <button className="countUpdater" onClick={() => setCount(++count)}>
+        Update Count
+      </button>
+      <button className="dayUpdater" onClick={() => setMorning(!isMorning)}>
+        Update Day
+      </button>
     </div>
   );
 };
