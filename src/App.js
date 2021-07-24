7ffeb53929b23414a61c0581ad67a5a402fbd98a
muhammadshahbaz08 <muhamadshahbaz17@gmail.com>
2021-07-24 23:20:46 +0500

    Adding CSS to App & Adding Day/Night Functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
 import { Message } from "./message";
+import './App.css'
 
 export const App = () => {
   let [count, setCount] = useState(0);
+  let [isMorning, setMorning] = useState(true);
   return (
-    <div className="box">
+    <div className={`box ${isMorning ? 'dayTime' : ''}`}>
+      <h1>Good {isMorning ? 'Morning' : 'Night'}</h1>
       <Message counter={count}/>
       <button onClick={() => setCount(++count)}>Update Count</button>
+      <button onClick={() => setMorning(!isMorning)}>Update Day</button>
     </div>
   );
 };
